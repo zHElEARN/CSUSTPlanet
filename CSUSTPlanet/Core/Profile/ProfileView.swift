d3f4dcbc1fc61c76a1fced03b5694393f6df8bf5
zHElEARN <personal@zhelearn.com>
2026-02-11 02:12:31 +0800

    refactor(个人详细): 优化刷新按钮显示

--- CSUSTPlanet/Core/Profile/ProfileView.swift
+++ CSUSTPlanet/Core/Profile/ProfileView.swift
@@ -179,7 +179,7 @@ struct ProfileView: View {
                 }
             }
             .navigationTitle("我的")
-            .toolbarTitleDisplayMode(.inline)
+            // .toolbarTitleDisplayMode(.inline)
             .sheet(isPresented: $viewModel.isLoginSheetPresented) {
                 SSOLoginView(isShowingLoginSheet: $viewModel.isLoginSheetPresented)
             }
@@ -208,6 +208,55 @@ struct ProfileView: View {
         .tabItem {
             Image(uiImage: UIImage(systemName: "person")!)
             Text("我的")
+        }
+    }
+}
+
+private struct ColoredLabel: View {
+    let title: String
+    let description: String?
+    let iconName: String?
+    let color: Color
+    let textColor: Color
+
+    init(title: String, iconName: String? = nil, color: Color? = nil, textColor: Color? = nil, description: String? = nil) {
+        self.title = title
+        self.iconName = iconName
+        self.color = color ?? .blue
+        self.textColor = textColor ?? .primary
+        self.description = description
+    }
+
+    private var labelTitle: some View {
+        VStack(alignment: .leading) {
+            Text(title)
+                .foregroundColor(textColor)
+            if let description = description {
+                Text(description)
+                    .font(.caption)
+                    .foregroundColor(textColor.opacity(0.7))
+                    .lineLimit(nil)
+                    .fixedSize(horizontal: false, vertical: true)
+            }
+        }
+    }
+
+    var body: some View {
+        if let iconName = iconName {
+            Label {
+                labelTitle
+            } icon: {
+                Image(systemName: iconName)
+                    .font(.system(size: 13, weight: .semibold))
+                    .foregroundColor(.white)
+                    .frame(width: 29, height: 29)
+                    .background(
+                        RoundedRectangle(cornerRadius: 6, style: .continuous)
+                            .fill(color.gradient)
+                    )
+            }
+        } else {
+            labelTitle
         }
     }
 }
