30b56725f4a76e739791f21b89fd53ed15fa33da
zHElEARN <personal@zhelearn.com>
2026-02-12 03:08:35 +0800

    refactor: 提取颜色辅助函数

--- CSUSTPlanet/Core/Features/FeaturesView.swift
+++ CSUSTPlanet/Core/Features/FeaturesView.swift
@@ -450,25 +450,6 @@ private struct AnnualReviewBanner: View {
     }
 }
 
-extension Color {
-    init(hex: String) {
-        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
-        var int: UInt64 = 0
-        Scanner(string: hex).scanHexInt64(&int)
-        let a: UInt64
-        let r: UInt64
-        let g: UInt64
-        let b: UInt64
-        switch hex.count {
-        case 3: (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
-        case 6: (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
-        case 8: (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
-        default: (a, r, g, b) = (1, 1, 1, 0)
-        }
-        self.init(.sRGB, red: Double(r) / 255, green: Double(g) / 255, blue: Double(b) / 255, opacity: Double(a) / 255)
-    }
-}
-
 // MARK: - Preview
 #Preview {
     NavigationView {
