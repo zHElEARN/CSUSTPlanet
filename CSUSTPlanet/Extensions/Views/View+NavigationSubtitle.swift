ab91d36f3d9fdd1a0b8387ea1b0987e438eba61c
zHElEARN <personal@zhelearn.com>
2026-03-18 16:23:24 +0800

    feat: 添加View相关扩展

---
+++ CSUSTPlanet/Extensions/Views/View+NavigationSubtitle.swift
@@ -0,0 +1,19 @@
+//
+//  View+NavigationSubtitle.swift
+//  CSUSTPlanet
+//
+//  Created by Claude on 2026/3/18.
+//
+
+import SwiftUI
+
+extension View {
+    @ViewBuilder
+    func navigationSubtitleCompat(_ subtitle: String) -> some View {
+        if #available(iOS 26.0, *) {
+            self.navigationSubtitle(subtitle)
+        } else {
+            self
+        }
+    }
+}
